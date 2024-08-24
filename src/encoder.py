e2f735e89ec029cfafcec0471980457373d60b23
tural <asgarovtural.0@gmail.com>
2024-08-24 23:22:31 +0400

    Input Embeddings

--- src/encoder.py
+++ src/encoder.py
@@ -1,1 +1,33 @@
+import torch
+from torch import nn
+import numpy as np
 
+class InputLayer(nn.Module):
+    
+    def __init__(self, d_model, vocabulary_size):
+        super().__init__()
+        self.d_model = d_model
+        self.vocabulary_size = vocabulary_size
+        self.embedding = nn.Embedding(vocabulary_size, d_model)
+
+    def forward(self):
+        return self.embedding * np.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, seq_len, dropout):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        pr = torch.zeros(seq_len, d_model)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = []
+
+
+
+print(
+    torch.arange(0, 512)
+    ) 
